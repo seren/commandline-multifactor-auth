3c12a4e7c8edc7c904bfa6f0edb8963e503406ed
Seren <seren@mellmo.com>
2012-11-21 13:38:21 -0800

    Complete refactor, including changing secrets from hash to array to support indexed reference.

--- mfa.rb
+++ mfa.rb
@@ -3,15 +3,19 @@
 # License: MIT
 
 
+# Note: QR code url: otpauth://totp/DESCRIPTION@mellmo?secret=SECRET
+
+
 require "base32"
 require "openssl"
 
+
 # Secrets from gmail, aws, wordpress, ssh, lastpass, dreamhost, cpanel, etc
-secrets={
-"b@gmail" => "CSWKEH3YUILXYCEU2V7T5GNWNM2PAW4V2ZHFOW6JLW6MEGY2OGJO7RIIQ37IEI3D",
-"bobby@gmail" => "7I4IW6KYA7JXNUQ55A33FNPHEVVVAOWJ2BWNV6ZMKYWMFRLLUPO5AFDL2RCDR77F",
-"bob@aws" => "WI27ZBHPOF4IAZRPOCZKAPDNRZHPCB6ECWSMWJQGCWRVOGVUVC3WBMJI5NFFMG2B",
-}
+secrets=[
+["b@gmail", "CSWKEH3YUILXYCEU2V7T5GNWNM2PAW4V2ZHFOW6JLW6MEGY2OGJO7RIIQ37IEI3D"],
+["bobby@gmail", "7I4IW6KYA7JXNUQ55A33FNPHEVVVAOWJ2BWNV6ZMKYWMFRLLUPO5AFDL2RCDR77F"],
+["bob@aws", "WI27ZBHPOF4IAZRPOCZKAPDNRZHPCB6ECWSMWJQGCWRVOGVUVC3WBMJI5NFFMG2B"]
+]
 
 
 @interval = 30
@@ -62,45 +66,87 @@ def secret_valid?(name,secret)
 end
 
 
+def format_opt (int)
+  "%06d" % int
+end
+
+
 # Check for typos in our secrets firsst
-secrets.each { |k,v| secret_valid?(k,v) }
+def check_for_typos(secrets)
+  secrets.each { |s| secret_valid?(s[0], s[1]) }
+end
 
-# If no arguments, output all OTPs
-if ARGV.empty?
-  secrets.each do |k,v|
-    puts "%06d %s" % [generate_otp(timecode(Time.now),v), k]
+
+def print_all(secrets)
+  secrets.each do |s|
+    puts "%06d  %s" % [generate_otp(timecode(Time.now),s[1]), s[0]]
   end
+end
 
-# If arguments, output matching OPTs ordered my match closness, and copy the first match to the clipboard (on OS X)
-else
+
+def print_all_with_index(secrets)
+  secrets.each_with_index do |s,i|
+    puts "%06d  %s - %s" % [generate_otp(timecode(Time.now),s[1]), i, s[0]]
+  end
+end
+
+
+def print_by_index_number(i, secrets)
+  secret = secrets[ARGV[0].to_i]
+  otp = format_opt( generate_otp(timecode(Time.now),secret[1]) )
+  `echo #{otp} | pbcopy`
+  puts ("#{otp} #{secret[0]}  <-- copied to clipboard")
+end
+
+
+def print_scored_matches(secrets_with_score)
+  # Sort the matched secrets
+  secrets_with_score_sorted = secrets_with_score.sort { |a,b| a[2]<=>b[2] }
+
+  # Copy first match to clipboard
+  copied_secret = secrets_with_score_sorted.first[1]
+  otp = format_opt( generate_otp(timecode(Time.now),copied_secret) )
+  `echo #{otp} | pbcopy`
+
+  first=true
+  secrets_with_score_sorted.each do |s|
+    otp = format_opt( generate_otp(timecode(Time.now),s[1]) )
+    print("#{otp} #{s[0]}")
+    if first
+      print(" <-- copied to clipboard\n")
+      first=false
+    else
+      puts
+    end
+  end
+end
+
+
+def args_match?(arg0, secrets)
   reg = Regexp.new("^"+ARGV[0]+"(.*)")
   # Builds a hash of secrets (where the key matched the regexp), consisting of: the key, the secret, and regexp score
-  secrets_with_score = secrets.reduce({}) do |a,(k,v)|
-    r = reg.match(k)
+  matches = secrets.select { |s| reg.match(s[1]) }
+end
+
+
+def score_matches(arg0, secrets)
+  reg = Regexp.new("^"+ARGV[0]+"(.*)")
+  # Builds a hash of secrets (where the key matched the regexp), consisting of: the key, the secret, and regexp score
+
+  secrets_with_score = secrets.map do |s|
+    r = reg.match(s[0])
     if r.nil?
-      a
+      nil
     else
       score = r[1]
-      a[k]={:secret => v, :score => score}
-      a
+      [s[0],s[1],score]
     end
-  end
+  end.compact
+end
 
-  unless secrets_with_score.empty?
-    secrets_with_score_sorted = secrets_with_score.sort { |a,b| a[1][:score]<=>b[1][:score] }
 
-    # Copy first match to clipboard
-    copied_secret = secrets_with_score_sorted.first[1][:secret]
-    otp = format_opt( generate_otp(timecode(Time.now),copied_secret) )
-    `echo #{otp} | pbcopy`
-
-    secrets_with_score_sorted.each do |k,v|
-      otp = format_opt( generate_otp(timecode(Time.now),v[:secret]) )
-      print("#{otp} #{k}")
-      (k == secrets_with_score_sorted.first.first) ? puts(" <-- copied to clipboard") : puts("")
-    end
- end
-
+def numeric?(object)
+  true if Float(object) rescue false
 end
 
 
@@ -110,13 +156,25 @@ end
 
 
 
+check_for_typos(secrets)
 
 
-
-
-
-
-
+# If no arguments, output all OTPs
+if ARGV.empty?
+  print_all_with_index(secrets)
+else
+  # See if arg matches any of the secrets' descriptions
+  secrets_with_score = score_matches(ARGV[0], secrets)
+  # If nothing matched, the arg is either an index number or just wrong.
+  unless secrets_with_score.empty?
+    print_scored_matches(secrets_with_score) 
+  else
+    # Check to see if the arg is a possible index.
+    if numeric?(ARGV[0]) && (ARGV[0].to_i <= secrets.count)
+      print_by_index_number(ARGV[0].to_i, secrets)
+    end
+  end
+end
 
 
 
